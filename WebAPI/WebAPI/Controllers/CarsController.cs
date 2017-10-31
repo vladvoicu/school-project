6668b7ce236ae9e218de8b44c72f216997e99a0c
Voicu Vlad <vladvoicu2811@gmail.com>
2017-10-31 10:13:07 +0200

    beggining backend

---
+++ WebAPI/WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CarDataAccess;
+
+namespace WebAPI.Controllers
+{
+    public class CarsController : ApiController
+    {
+        public IEnumerable<Car> Get()
+        {
+            using (CarsDBEntities entities = new CarsDBEntities())
+            {
+                return entities.Cars.ToList();
+            }
+        }
+
+        public Car Get(int id)
+        {
+            using (CarsDBEntities entities = new CarsDBEntities())
+            {
+                return entities.Cars.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
